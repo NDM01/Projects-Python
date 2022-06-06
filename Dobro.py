4e20c5fca34fe18e4a14e5e45f406c4ad28624bc
NDM01 <2004ndof@gmail.com>
2022-06-06 16:40:26 +0100

    projects

---
+++ Dobro.py
@@ -0,0 +1,3 @@
+x=int(input("Insira o número: "))
+dobro=x*2
+print("O dobro do número é: " + str(dobro))
---
+++ Max-Min.py
@@ -0,0 +1,10 @@
+x=int(input("Insira o primeiro número: "))
+y=int(input("Insira o segundo número: "))
+z=int(input("Insira o treceiro número: "))
+x=int(input("Insira o quarto número: "))
+list1=[x,y,z,x]
+m=max(list1)
+n=min(list1)
+print("O maior número é: " +str(m))
+print("O menor número é: " +str(n))
+
