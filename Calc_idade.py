ced5ee2776270be390a44bc4fa60c5c4e97a657c
NDM01 <2004ndof@gmail.com>
2022-06-15 15:59:16 +0100

    Calc_Idade

---
+++ Calc_idade.py
@@ -0,0 +1,23 @@
+from datetime import date 
+
+teste = int(input("Insira o ano: "))
+mes = int(input("Insira o mes: "))
+dia = int(input("Insira o dia: "))
+  
+def calculateAge(born): 
+    today = date.today() 
+    try:  
+        birthday = born.replace(year = today.year) 
+  
+    
+    
+    except ValueError:  
+        birthday = born.replace(year = today.year, 
+                  month = born.month + 1, day = 1) 
+  
+    if birthday > today: 
+        return today.year - born.year - 1
+    else: 
+        return today.year - born.year 
+          
+print(calculateAge(date(teste, mes, dia)), "years")
