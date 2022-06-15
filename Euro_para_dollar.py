cb5a08beaea314e4536bdce9f959169a456d6e75
NDM01 <2004ndof@gmail.com>
2022-06-15 14:39:29 +0100

    Euro_para_dollar

---
+++ Euro_para_dollar.py
@@ -0,0 +1,15 @@
+from time import sleep
+
+dollar = 0,96 
+
+val = 0
+
+eu = int(input("Insira a quantidade em euros: "))
+
+val = eu * dollar 
+
+print("A converter...")
+
+sleep(1)
+
+print("Valor em dollar: %s" %(val))
