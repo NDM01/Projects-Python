305894c69f92ecf12297adc5a791ecb9a5c9a511
NDM01 <2004ndof@gmail.com>
2022-06-17 13:03:26 +0100

    Jogo da forca v2.0

---
+++ Jogo_forcav2.py
@@ -0,0 +1,110 @@
+from time import sleep
+import random
+
+erro = 1
+
+def acertou_palavra():
+    print("Parabén acertou na palavra!!")
+    print(" :)")
+    sleep
+    quit()
+
+def mensagem_abertura():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca v2.0!***")
+    print("*********************************")
+
+mensagem_abertura()
+
+print("A gerar palavra...")
+
+sleep(1)
+
+lista = ['gato','escola','kiwi','hospital','aluno','cola','pneu','barco','garraga','monitor','quadro','estrada','caneta','computador','carteira','mesa','carro','galinha','casa','mota','capacete','lancheira','janela','telhado','borracha','churrasco','luz','estojo']
+
+pal = random.choice(lista)
+
+print(pal)
+
+print("Lista de palavras:\n %s " %(lista))
+
+resp = input("Insira a resposta: ")
+
+while resp != pal:
+    print("Errado!")
+    resp = input("Insira a resposta: ")
+    if resp != pal:
+        erro = erro + 1
+    if erro == 1:
+        print("  _______     ")
+        print(" |/      |    ")
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+        print("_|___         ")
+    elif erro == 2:
+        print("  _______     ")
+        print(" |/      |    ")
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+        print("_|___         ")
+    elif erro == 3:
+        print("  _______     ")
+        print(" |/      |    ")
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+        print("_|___         ")
+    elif erro == 4:
+        print("  _______     ")
+        print(" |/      |    ")
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+        print("_|___         ")
+    elif erro == 5:
+        print("  _______     ")
+        print(" |/      |    ")
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+        print(" |            ")
+        print("_|___         ")
+    elif erro == 6:
+        print("  _______     ")
+        print(" |/      |    ")
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+        print(" |            ")
+        print("_|___         ")
+    elif erro == 7:
+        print("  _______     ")
+        print(" |/      |    ")
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+        print(" |            ")
+        print("_|___         ")
+        print("Perdeu!")
+        quit()
+        
+if resp == pal:
+#    print("Parabéns acertou!")  
+    acertou_palavra()  
+
+
+
+
