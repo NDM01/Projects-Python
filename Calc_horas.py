6ca51b52a3eb52cf0d298e4fd1d2d8434bcc7647
NDM01 <2004ndof@gmail.com>
2022-06-17 10:04:55 +0100

    Calcular horas

---
+++ Calc_horas.py
@@ -0,0 +1,11 @@
+from time import sleep
+
+dias = int(input("Insira o número de dias que já trabalhou: "))
+
+print("A calcular...")
+
+sleep(1)
+
+x = dias * 8
+
+print("Já trabalhou %s horas" %(x))
