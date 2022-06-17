91157fcca81e44d979f78383ba88bf2ebf73cec6
NDM01 <2004ndof@gmail.com>
2022-06-17 14:37:38 +0100

    Desconto

---
+++ Desconto.py
@@ -0,0 +1,12 @@
+from time import sleep
+
+prom = '25%'
+
+valor = int(input("Insira o montante da compra: "))
+
+if valor >= 100:
+    desc = valor * 0.25 
+    total = valor - desc
+    print("A compra ultrapassa os 100€, portanto tem direito a %s de desconto\nFicando por: %s€" %(prom,total))
+else:
+    print("A compra não ultrapassa os 100€, não tendo direito a desconto\nFicando por: %s€" %(valor))
