aabf6fbc4766ab7b22e52b9284eeaeef3210b690
NDM01 <2004ndof@gmail.com>
2022-06-22 15:01:17 +0100

    Aula2

---
+++ soma_def.py
@@ -0,0 +1,11 @@
+# Exercício proposto do curso
+
+def so(n1,n2,n3):
+    soma = n1 + n2 + n3
+    print(f'A soma dos valores é {soma}')
+    return
+
+num1 = int(input('Insira o primeiro valor: '))
+num2 = int(input('Insira o segundo valor: '))
+num3 = int(input('Insira o terceiro valor: '))
+so(n1 = num1, n2= num2, n3= num3)
