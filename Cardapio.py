3c0d99f7788b07520f9f226a45bd4a0fc6c2b0dc
NDMO1 <2004ndof@gmail.com>
2022-10-14 01:17:13 +0100

    Cardapio

--- Cardapio.py
+++ Cardapio.py
@@ -1,17 +1,34 @@
-from ast import While
+from ast import Return, While
+from time import sleep
 
+l = True
 
+print('--------------------------')
 print('Menu disponíveis: ')
 print('1 -- Pizza 2,50€')
 print('2 -- Doublecheese 3,50€')
 print('3 -- Batata 1,50€')
 print('4 -- Sumo 1€')
+print('--------------------------')
 
-while True:
+while l==True:
     sel = input('Insira o produto que pretende: ')
-    if not sel.isnumeric:
+    if not sel.isnumeric():
         print('Produto inválido!')
         print('Insira o número de um produto válido!')
-        quit() 
+        # return
+    sel=int(sel)    
+    if sel > 4:
+        print('Produto inválido!')
+        print('Insira o número de um produto válido!')
+        Return
+
+    # else:
+
+    #     print('teste')
+    #     l = False
+
+        
+    
 
 print()
