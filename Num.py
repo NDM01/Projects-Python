930abdb66b04e707fc8c70de4515423228afd467
Nelson França <1221527@isep.ipp.pt>
2022-10-20 19:49:49 +0100

    Num

---
+++ Num.py
@@ -0,0 +1,19 @@
+from ast import For, While
+
+print('Insira a quantida de números: ')
+
+N = int(input())
+
+while N < 0:
+        print('Número inválido!')
+
+        print('Insira o número: ')
+
+        N = int(input())
+
+I = 0
+
+for I in range(N):
+    
+        If
+    
