dba37966fe3ecb8e5ddfae2fcf28d288bfe78944
Nelson França <1221527@isep.ipp.pt>
2022-10-20 21:53:49 +0100

    Num

--- Num.py
+++ Num.py
@@ -8,11 +8,11 @@ print('Insira a quantida de números: ')
 N = int(input())
 
 while N < 0:
-        print('Número inválido!')
+  print('Número inválido!')
 
-        print('Insira o número: ')
+  print('Insira o número: ')
 
-        N = int(input())
+  N = int(input())
 
 I = 0
 
@@ -25,7 +25,7 @@ for I in range(N):
     Somapos = Somapos + 1
 
 if Soma > 0:
-    perc = Somaneg / Soma
+  perc = Somaneg / Soma
 
 percp = Somapos / N * 100
 
