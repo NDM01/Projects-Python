42220471f481e23209335fabad0c9e3f31f33058
Nelson França <1221527@isep.ipp.pt>
2023-01-26 22:24:14 +0000

    Main menu (v1)

--- Aeroporto.py
+++ Aeroporto.py
@@ -1,1 +1,14 @@
-print("Bem-Vindo!")
+import os
+
+def menu():
+    
+    print("1 - Consultar Voos")
+    print("2 - Consultar Aviões")
+    print("3 - Consultar Aeroportos")
+    print("4 - Consultar Pilotos")
+    print("5 - Consultar Tripulação")
+    print("6 - Consultar Passageiros")
+    print("7 - Sair")
+
+
+print("Bem-Vindo!") 
